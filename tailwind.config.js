5c7f1b96d4244177f9cd68d974e7fc253f1b0e45
did <did@Mac.home>
2025-03-19 05:19:25 +0100

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./**/*.{html,md,js}", // adjust these paths if needed
+        "./**/*.{html,md,js,liquid}",
         "./*.{html,md,js}",
-        "./_includes/*.html",
-        "./_layouts/*.html"
+        "./_includes/*.{html,md,js,liquid}",
+        "./_layouts/*.{html,md,js,liquid}"
+    ],
+    safelist: [
+        // Include classes that are dynamically generated or not explicitly found in the files
+        'block',
+        'hidden',
+        'bg-green-500',
+        'hover:bg-green-600',
+        'dark:bg-gray-900',
+        'dark:border-gray-700',
+        // Add any additional classes you use in a dynamic context
     ],
     theme: {
         extend: {},
