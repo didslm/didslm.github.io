9e309a7302196f9284796d2d1e29e74606c32fc5
did <did@Mac.home>
2025-03-19 04:45:15 +0100

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
     content: [
         "./**/*.{html,md,js}", // adjust these paths if needed
-        "./*.{html,md,js}"
+        "./*.{html,md,js}",
+        "./_includes/*.html",
+        "./_layouts/*.html"
     ],
     theme: {
         extend: {},
     },
     plugins: [],
+    important: true,
 }
